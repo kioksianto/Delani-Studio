99df9eaaa2ae9eaf19e51926db2a4e37bf5deb4a
kioksianto <brenda.sianto@gmail.com>
2021-03-05 12:12:26 +0300

    javascript

---
+++ script.js
@@ -0,0 +1,52 @@
+
+//Overlay style on What We Do section on Website
+
+$(document).ready(function(){
+
+    // Design Description
+    $('#desc-image-1').click(function(){
+        $('#desc-1').show();
+        $('#desc-image-1').hide();
+
+    })
+
+    $('#desc-1').click(function(){
+        $('#desc-image-1').show();
+        $('#desc-1').hide();
+
+    })
+
+    // Development Description
+    $('#desc-image-2').click(function(){
+        $('#desc-2').show();
+        $('#desc-image-2').hide();
+
+    })
+
+    $('#desc-2').click(function(){
+        $('#desc-image-2').show();
+        $('#desc-2').hide();
+
+    })
+    // Product Design Description
+    $('#desc-image-3').click(function(){
+        $('#desc-3').show();
+        $('#desc-image-3').hide();
+
+    })
+
+    $('#desc-3').click(function(){
+        $('#desc-image-3').show();
+        $('#desc-3').hide();
+
+    })
+    
+
+});
+
+
+//Message alert after subscription
+function setName(){
+    var name = document.getElementById('mce-NAME');
+    alert (name + ",we have received your message.");
+}
